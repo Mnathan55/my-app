0f954e4463d7230ed12781b06510a79cca3bba8a
Mangesh <mangeshsirsat10@gmail.com>
2025-08-14 14:23:59 +0530

    fixed

--- src/app/api/wallet/route.ts
+++ src/app/api/wallet/route.ts
@@ -1,8 +1,8 @@
 // src/app/api/wallets/route.ts
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/authOption";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { authOptions } from "@/lib/authOption";
 
 // Fetch wallets
 export async function GET() {
